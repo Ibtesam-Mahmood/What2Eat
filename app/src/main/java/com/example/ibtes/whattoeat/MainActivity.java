b7c3c206c085a01ef005cc24ac2cd3c8c18037c6
Ibtesam Mahmood <ibtesam.mahmood2@gmail.com>
2018-03-18 20:32:26 -0400

    Basic Location functionality added

--- app/src/main/java/com/example/ibtes/whattoeat/MainActivity.java
+++ app/src/main/java/com/example/ibtes/whattoeat/MainActivity.java
@@ -1,11 +1,22 @@
 package com.example.ibtes.whattoeat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,12 +26,51 @@ public class MainActivity extends AppCompatActivity {
     private RadioButton foodType = null;
     private RadioButton price = null;
 
+    private FusedLocationProviderClient mFusedLocationClient;
+
+    private Location userLocation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         displayText = findViewById(R.id.displayText);
+
+        getLocation();
+    }
+
+    private void getLocation(){
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        //Checks if the permission is granted
+        if ( ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    1);
+            getLocation();
+            return;
+
+        }
+        else{ //If the permission is granted
+
+            mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if(location != null){
+                                TextView locationName = findViewById(R.id.locationText);
+
+                                locationName.setText("We gucci");
+
+                                userLocation = location;
+                            }
+                        }
+                    });
+        }
+
     }
 
     public void onFoodTypeClick(View view){
@@ -42,7 +92,8 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
-        //Conditional Logic to find location
+        //Conditional Logic to find food
+
 
         foodType.setChecked(false);
         price.setChecked(false);
