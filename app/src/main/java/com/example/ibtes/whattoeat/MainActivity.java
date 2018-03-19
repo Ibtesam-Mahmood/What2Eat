e91a88733a2e1fc58d3355b0fc3e4823fa4118f3
Ibtesam Mahmood <ibtesam.mahmood2@gmail.com>
2018-03-18 20:16:14 -0400

    Basic conditional logic added

--- app/src/main/java/com/example/ibtes/whattoeat/MainActivity.java
+++ app/src/main/java/com/example/ibtes/whattoeat/MainActivity.java
@@ -2,12 +2,54 @@ package com.example.ibtes.whattoeat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private TextView displayText;
+
+    private RadioButton foodType = null;
+    private RadioButton price = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        displayText = findViewById(R.id.displayText);
     }
+
+    public void onFoodTypeClick(View view){
+
+        foodType = (RadioButton) view;
+
+    }
+
+    public void onPriceClick(View view){
+
+        price = (RadioButton) view;
+
+    }
+
+    public void search(View view){
+
+        if(foodType == null || price == null){
+            Toast.makeText(this, "Please select a price and type", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //Conditional Logic to find location
+
+        foodType.setChecked(false);
+        price.setChecked(false);
+
+        foodType = null;
+        price = null;
+
+    }
+
 }
