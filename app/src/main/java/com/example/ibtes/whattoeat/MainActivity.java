dfa47c1fe779c6435025c1a11ad3152319f86c3c
Ibtesam Mahmood <ibtesam.mahmood2@gmail.com>
2018-03-18 20:41:41 -0400

    Basic yelp functionality added

--- app/src/main/java/com/example/ibtes/whattoeat/MainActivity.java
+++ app/src/main/java/com/example/ibtes/whattoeat/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.RadioButton;
 import android.widget.TextView;
@@ -15,8 +16,21 @@ import android.widget.Toast;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.yelp.fusion.client.connection.YelpFusionApi;
+import com.yelp.fusion.client.connection.YelpFusionApiFactory;
+import com.yelp.fusion.client.models.Business;
+import com.yelp.fusion.client.models.Coordinates;
+import com.yelp.fusion.client.models.SearchResponse;
 
 import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
 
     private Location userLocation;
 
+    private YelpFusionApi yelpFusionApi;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +54,16 @@ public class MainActivity extends AppCompatActivity {
         displayText = findViewById(R.id.displayText);
 
         getLocation();
+
+        String yelpApiKey = getString(R.string.yelpApiKey);
+
+        try {
+            yelpFusionApi =  new YelpFusionApiFactory().createAPI(yelpApiKey);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
     private void getLocation(){
@@ -73,6 +99,42 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public String yelpSearch(String term){
+
+        String reply = null;
+
+        Map<String, String> params =  new HashMap<>();
+
+        params.put("term", term);
+        params.put("latitude", userLocation.getLatitude() + "");
+        params.put("longitude", userLocation.getLongitude() + "");
+
+        Call<SearchResponse> call = yelpFusionApi.getBusinessSearch(params);
+
+        Callback<SearchResponse> callback = new Callback<SearchResponse>() {
+            @Override
+            public void onResponse(Call<SearchResponse> call, Response<SearchResponse> response) {
+
+                SearchResponse searchResponse = response.body();
+
+                Business result = searchResponse.getBusinesses().get(0);
+
+                reply = result.getName();.
+
+
+            }
+            @Override
+            public void onFailure(Call<SearchResponse> call, Throwable t) {
+
+            }
+        };
+
+        call.enqueue(callback);
+
+        return reply;
+
+    }
+
     public void onFoodTypeClick(View view){
 
         foodType = (RadioButton) view;
@@ -94,6 +156,14 @@ public class MainActivity extends AppCompatActivity {
 
         //Conditional Logic to find food
 
+        String location = yelpSearch(foodType.getText().toString());
+
+        if(location == null){
+            Toast.makeText(this, "Search Failed", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            displayText.setText(location);
+        }
 
         foodType.setChecked(false);
         price.setChecked(false);
